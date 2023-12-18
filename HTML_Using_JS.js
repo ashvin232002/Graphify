8464a35d86bcf80aeb57a4bceb384e7aa707d0bb
Ashvin Kumhar <120255875+ashvin232002@users.noreply.github.com>
2023-12-18 21:52:30 +0530

    Update HTML_Using_JS.js

--- HTML_Using_JS.js
+++ HTML_Using_JS.js
@@ -1,4 +1,41 @@
-var x='<div class="d-flex p-2"><div></div><div class="key wall"></div><div> Wall</div></div><div></div><div class="d-flex p-2"><div></div><div class="key start"></div><div>Start</div><div></div></div><div class="d-flex p-2"><div></div><div class="key end"></div><div>Target</div></div><div class="d-flex p-2"><div></div><div class="key searching"></div>';
-var y='<div class="key visited"></div><div>Visited</div></div><div class="d-flex p-2"><div class="key success"></div><div></div><div>Path/Shortest Path</div></div><div class="d-flex p-2"><div class="key"></div><div></div><div>Unvisited</div></div>';
+var x = `
+  <div class="d-flex p-2">
+    <div></div>
+    <div class="key wall"></div>
+    <div> Wall</div>
+  </div>
+  <div></div>
+  <div class="d-flex p-2">
+    <div></div>
+    <div class="key start"></div>
+    <div>Start</div>
+    <div></div>
+  </div>
+  <div class="d-flex p-2">
+    <div></div>
+    <div class="key end"></div>
+    <div>Target</div>
+  </div>
+  <div class="d-flex p-2">
+    <div></div>
+    <div class="key searching"></div>
+  `;
+
+var y = `
+  <div class="key visited"></div>
+  <div>Visited</div>
+  </div>
+  <div class="d-flex p-2">
+    <div class="key success"></div>
+    <div></div>
+    <div>Path/Shortest Path</div>
+  </div>
+  <div class="d-flex p-2">
+    <div class="key"></div>
+    <div></div>
+    <div>Unvisited</div>
+  </div>
+`;
+
 document.write(x);
 document.write(y);
